8572e1de3723f94058a76d03b94c710a045ffa05
riteshadhikari05 <02250365.cst@rub.edu.bt>
2025-08-11 23:43:10 +0530

    variables_and_data_exercise

---
+++ python_exercises/basics/data_structures/dictionaries.py
@@ -0,0 +1,17 @@
+name = "Ritesh Adhikari"
+age= 19
+height = 1.80
+is_student = True
+person_info = {
+    "name": name,
+    "age": age,
+    "height": height,
+    "is_student": is_student
+}
+print(person_info)
+person_info["favorite_color"] = "blue"  # Replace with your actual favorite color
+print(person_info)
+try:
+    print(person_info["weight"])
+except KeyError as e:
+    print(f"Error: {e}")
